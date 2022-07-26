d0898961d8595110fd76db97bff1827ba58aaed2
dmitryy <dmitry@ruswizards.com>
2022-07-26 20:20:41 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import { MenuItem, Select } from '@mui/material';
+
+function App() {
+  return (
+<div className="App" style={{ border: '2px solid red' }}>
+      <br/><br/><br/><br/><br/>
+      <span>1 of 6</span>
+      <Select>
+        <MenuItem value="1">One</MenuItem>
+        <MenuItem value="2">Two</MenuItem>
+        <MenuItem value="3">Three</MenuItem>
+        <MenuItem value="4">Four</MenuItem>
+        <MenuItem value="5">Five</MenuItem>
+      </Select>
+      <br/><br/><br/><br/><br/>
+      <hr/>
+      <span>2 of 6</span>
+      <Select>
+        <MenuItem value="1">One</MenuItem>
+        <MenuItem value="2">Two</MenuItem>
+        <MenuItem value="3">Three</MenuItem>
+        <MenuItem value="4">Four</MenuItem>
+        <MenuItem value="5">Five</MenuItem>
+      </Select>
+      <br/><br/><br/><br/><br/>
+      <hr/>
+      <span>3 of 6</span>
+      <Select>
+        <MenuItem value="1">One</MenuItem>
+        <MenuItem value="2">Two</MenuItem>
+        <MenuItem value="3">Three</MenuItem>
+        <MenuItem value="4">Four</MenuItem>
+        <MenuItem value="5">Five</MenuItem>
+      </Select>
+      <br/><br/><br/><br/><br/>
+      <hr/>
+      <span>4 of 6</span>
+      <Select>
+        <MenuItem value="1">One</MenuItem>
+        <MenuItem value="2">Two</MenuItem>
+        <MenuItem value="3">Three</MenuItem>
+        <MenuItem value="4">Four</MenuItem>
+        <MenuItem value="5">Five</MenuItem>
+      </Select>
+      <br/><br/><br/><br/><br/>
+      <hr/>
+      <span>5 of 6</span>
+      <Select>
+        <MenuItem value="1">One</MenuItem>
+        <MenuItem value="2">Two</MenuItem>
+        <MenuItem value="3">Three</MenuItem>
+        <MenuItem value="4">Four</MenuItem>
+        <MenuItem value="5">Five</MenuItem>
+      </Select>
+      <br/><br/><br/><br/><br/>
+      <hr/>
+      <span>6 of 6</span>
+      <Select>
+        <MenuItem value="1">One</MenuItem>
+        <MenuItem value="2">Two</MenuItem>
+        <MenuItem value="3">Three</MenuItem>
+        <MenuItem value="4">Four</MenuItem>
+        <MenuItem value="5">Five</MenuItem>
+      </Select>
+      <br/><br/><br/><br/><br/>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
